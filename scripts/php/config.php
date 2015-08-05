d8b14fee7b9bf94dbae7ee17afb4af7f8643f58e
Tim Falken <timfalken@hotmail.com>
2015-08-05 13:09:09 +0200

    Set up config.php for database on timfalken.com

--- scripts/php/config.php
+++ scripts/php/config.php
@@ -1,6 +1,18 @@
 <?php
-	$serverName = "localhost";
-	$databaseName = "reservering";
-	$databaseUserName = "root";
-	$databasePassword = "";
+	$online = true;
+
+	if(!$online)
+	{
+		$serverName = "localhost";
+		$databaseName = "reservering";
+		$databaseUserName = "root";
+		$databasePassword = "";
+	}
+	else
+	{
+		$serverName = "db.timfalken.com";
+		$databaseName = "md300889db334728";
+		$databaseUserName = "md300889db334728";
+		$databasePassword = "tslDeRWQ";
+	}
 ?>
